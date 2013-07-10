e1c5db42b42d6002233351add4f19e502be3ee94
Lior Silberberg <lior.silberberg@gmail.com>
2013-07-10 22:10:37 +0000

    new

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   fs.readFileSync(index.html);
-  buf.toString();
+  buferf.toString();
 });
 
 var port = process.env.PORT || 5000;
