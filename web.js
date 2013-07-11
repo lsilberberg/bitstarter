e5edb73ffe009eeb4c4727fd34f6014249418bef
Lior Silberberg <lior.silberberg@gmail.com>
2013-07-11 10:51:41 +0000

    again

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var fs = require('fs');
-var buffer = require('buffer');
+var buf = new Buffer(fs.readFileSync('index.html', 'utf-8');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-   response.send(buf.toString(fs.readFileSync(index.html)););
+   response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
